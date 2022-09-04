d68ec412f73f1537b6985966c50c730fdea33520
qjvk2880 <qjvk2880@gmail.com>
2022-09-04 18:46:17 +0900

    first commit

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string.h> //strstr 함수가 선언된 헤더 파일
+#include <cstring> //c++일경우
+using namespace std;
+int main(){
+    cout << "hgddd";
+}
+    
